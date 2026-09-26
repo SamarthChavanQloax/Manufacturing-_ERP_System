07a785b87ceb19195c2a208c28f61dbf88704cbd
Samarth Chavan <samarthchavan70@gmail.com>
2026-09-26 12:39:23 +0530

    Untrack generated cache files from git

--- backend/dist/customers/customers.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from 'typeorm';
-import { Customer } from '../entities';
-export declare class CustomersService {
-    private customerRepo;
-    constructor(customerRepo: Repository<Customer>);
-    findAll(): Promise<Customer[]>;
-    create(customerName: string): Promise<Customer>;
-    update(id: number, customerName: string): Promise<Customer>;
-}
--- backend/dist/reports/reports.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { ReportsService } from './reports.service';
-export declare class ReportsController {
-    private reportsService;
-    constructor(reportsService: ReportsService);
-    getGateOutReport(): Promise<any[]>;
-}
